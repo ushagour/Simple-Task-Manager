f8c2f475994820e001e49d7e9a5983bd0e7cc9cd
ushagour <ali.ouchagour01@gmail.com>
2025-08-23 15:12:37 +0100

    simple changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,10 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App</code> and save to reload.
-        </p>
+ali         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
